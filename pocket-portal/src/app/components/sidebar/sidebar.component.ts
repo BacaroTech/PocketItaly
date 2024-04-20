495d94159ec46beb5a840124b70784b5aff3ceb4
scardummichele <scarpa.michele.90@gmail.com>
2024-04-20 12:38:44 +0200

    create screen

--- pocket-portal/src/app/components/sidebar/sidebar.component.ts
+++ pocket-portal/src/app/components/sidebar/sidebar.component.ts
@@ -1,19 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PMenuItem } from '../../interfaces/menu.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
 
-  menuList: string[] = [
-    "Dashboard",
-    "Map",
-    "Report",
-    "Product"
+  // menuList: string[] = [
+  //   "Dashboard",
+  //   "Map",
+  //   "Report",
+  //   "Product"
+  // ]
+  menuList: PMenuItem[] = [
+    {
+      label: "Dashboard",
+      id: "Dashboard",
+      route: "/dashboard"
+    },
+    {
+      label: "Map",
+      id: "Map",
+      route: "/map"
+    },
+    {
+      label: "Report",
+      id: "Report",
+      route: "/report"
+    }
   ]
 }
