23e5de2b29b76e61dba3763692a9998a38b728a0
scardummichele <scarpa.michele.90@gmail.com>
2024-04-20 17:12:38 +0200

    add table

---
+++ pocket-portal/src/app/components/pocket-table/pocket-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pocket-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './pocket-table.component.html',
+  styleUrl: './pocket-table.component.css'
+})
+export class PocketTableComponent {
+
+}
--- pocket-portal/src/app/screens/report/report.component.ts
+++ pocket-portal/src/app/screens/report/report.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PocketTableComponent } from '../../components/pocket-table/pocket-table.component';
 
 @Component({
   selector: 'app-report',
   standalone: true,
-  imports: [],
+  imports: [PocketTableComponent],
   templateUrl: './report.component.html',
   styleUrl: './report.component.css'
 })
